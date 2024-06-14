98d72a00e76f3227be916729b38a3a0105b6bcee
AmazingAaravCoding <142331565+AmazingAaravCoding@users.noreply.github.com>
2024-06-14 17:55:26 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 function check() {
-    speak()
+    img = document.getElementById("pic")
+    classifier.classify(img, gotresult)
 }
 function capture() {
     Webcam.snap(function (img) {
@@ -27,4 +28,34 @@ function speak() {
     speak2 = "and the second prediction is " + p2
     utter = new SpeechSynthesisUtterance(speak1 + speak2)
     synth.speak(utter)
+}
+function gotresult(error, result) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(result);
+        p1 = result[0].label
+        p2 = result[1].label
+        speak()
+        document.getElementById("emotion1").innerHTML = p1
+        document.getElementById("emotion2").innerHTML = p2
+        if (p1 == "Thumbs up") {
+            document.getElementById("emoji1").innerHTML = "👍"
+        }
+        if (p1 == "Crossed Fingers") {
+            document.getElementById("emoji1").innerHTML = "🤞"
+        }
+        if (p1 == "Palm") {
+            document.getElementById("emoji1").innerHTML = "🖐"
+        }
+        if (p2 == "Thumbs up") {
+            document.getElementById("emoji2").innerHTML = "👍"
+        }
+        if (p2 == "Crossed Fingers") {
+            document.getElementById("emoji2").innerHTML = "🤞"
+        }
+        if (p2 == "Palm") {
+            document.getElementById("emoji2").innerHTML = "🖐"
+        }
+    }
 }
